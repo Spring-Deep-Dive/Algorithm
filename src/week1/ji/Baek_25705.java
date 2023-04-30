637201a1290749a0fd60e277f00ef52b4dc5be61
33varme22 <85978996+jee14@users.noreply.github.com>
2023-04-30 17:43:25 +0900

    Update Baek_25705.java
    indentation 조정

--- src/week1/ji/Baek_25705.java
+++ src/week1/ji/Baek_25705.java
@@ -2,22 +2,22 @@ import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-		String[] wheelArr = sc.next().split("");
-		int m = sc.nextInt();
-		String[] targetArr =  sc.next().split("");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] wheelArr = sc.next().split("");
+        int m = sc.nextInt();
+        String[] targetArr = sc.next().split("");
 
-		int cnt = -1;
-		int idx = 0;
+        int cnt = -1;
+        int idx = 0;
 
-		while (cnt++ <= 10000 && idx < targetArr.length) {
-			if (targetArr[idx].equals(wheelArr[cnt % wheelArr.length])) {
-				idx++;
-			}
-		}
+        while (cnt++ <= 10000 && idx < targetArr.length) {
+            if (targetArr[idx].equals(wheelArr[cnt % wheelArr.length])) {
+                idx++;
+            }
+        }
 
-		System.out.println(cnt <= 10000 ? cnt : -1);
-	}
+        System.out.println(cnt <= 10000 ? cnt : -1);
+    }
 }
