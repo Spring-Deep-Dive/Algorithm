dd6af7d7b1e1e2c725382fc1661e2262739ccbc1
33varme22 <85978996+jee14@users.noreply.github.com>
2023-04-30 17:43:00 +0900

    Update Baek_1972.java
    indentation 조정

--- src/week1/ji/Baek_1972.java
+++ src/week1/ji/Baek_1972.java
@@ -2,42 +2,42 @@ import java.util.*;
 
 public class Main {
 
-	private static boolean flag;
-	private static String[] inputArr;
-	private static final List<String> list = new ArrayList<>();
+    private static boolean flag;
+    private static String[] inputArr;
+    private static final List<String> list = new ArrayList<>();
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String input;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input;
 
-		while (true) {
-			flag = true;
-			input = sc.next();
-			if ("*".equals(input)) {
-				break;
-			}
-			inputArr = input.split("");
-			if (inputArr.length > 1) {
-				validate();
-			}
-			print(input);
-		}
-	}
+        while (true) {
+            flag = true;
+            input = sc.next();
+            if ("*".equals(input)) {
+                break;
+            }
+            inputArr = input.split("");
+            if (inputArr.length > 1) {
+                validate();
+            }
+            print(input);
+        }
+    }
 
-	private static void validate() {
-		for (int i = 0; i < inputArr.length - 1 && flag; i++) {
-			for (int j = 0, k = j + i + 1; k < inputArr.length && flag; j++, k++) {
-				String tmp = inputArr[j] + inputArr[k];
-				if (list.contains(tmp)) {
-					flag = false;
-				}
-				list.add(tmp);
-			}
-			list.clear();
-		}
-	}
+    private static void validate() {
+        for (int i = 0; i < inputArr.length - 1 && flag; i++) {
+            for (int j = 0, k = j + i + 1; k < inputArr.length && flag; j++, k++) {
+                String tmp = inputArr[j] + inputArr[k];
+                if (list.contains(tmp)) {
+                    flag = false;
+                }
+                list.add(tmp);
+            }
+            list.clear();
+        }
+    }
 
-	private static void print(String str) {
-		System.out.println(str + " is" + (flag?" ":" NOT ") + "surprising.");
-	}
+    private static void print(String str) {
+        System.out.println(str + " is" + (flag ? " " : " NOT ") + "surprising.");
+    }
 }
