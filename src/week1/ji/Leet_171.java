6a08aeb63b0719f86af70a14aeb977be75092bd6
33varme22 <85978996+jee14@users.noreply.github.com>
2023-04-30 17:43:53 +0900

    Update Leet_171.java
    indentation 조정

--- src/week1/ji/Leet_171.java
+++ src/week1/ji/Leet_171.java
@@ -1,10 +1,10 @@
 class Solution {
-	public int titleToNumber(String columnTitle) {
-		int res = 0;
-		for (int i = 0, j = columnTitle.length() - 1; i < columnTitle.length(); i++, j--) {
-			res += (columnTitle.charAt(i) - 64) * (int) Math.pow(26, j);
-		}
+    public int titleToNumber(String columnTitle) {
+        int res = 0;
+        for (int i = 0, j = columnTitle.length() - 1; i < columnTitle.length(); i++, j--) {
+            res += (columnTitle.charAt(i) - 64) * (int)Math.pow(26, j);
+        }
 
-		return res;
-	}
+        return res;
+    }
 }
