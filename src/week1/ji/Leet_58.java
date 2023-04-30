0519acdaf32b4ecbe2a8c0ccd855f4613cbc4790
33varme22 <85978996+jee14@users.noreply.github.com>
2023-04-30 17:44:11 +0900

    Update Leet_58.java
    indentation 조정

--- src/week1/ji/Leet_58.java
+++ src/week1/ji/Leet_58.java
@@ -3,8 +3,8 @@ import java.util.Arrays;
 class Solution {
     public int lengthOfLastWord(String s) {
         return Arrays.stream(s.trim().split(" "))
-				.reduce((first, second) -> second)
-				.get()
-				.length();
+            .reduce((first, second) -> second)
+            .get()
+            .length();
     }
 }
