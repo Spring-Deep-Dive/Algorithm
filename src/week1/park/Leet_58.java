7978567bd9fdf6ba8a5534dd079c02655cb6f246
Joonwoo <93jpark@gmail.com>
2023-05-02 19:59:54 +0900

    solved

--- src/week1/park/Leet_58.java
+++ src/week1/park/Leet_58.java
@@ -1,3 +1,4 @@
+import java.util.StringTokenizer;
 
 /**
  * week 1 String
@@ -5,7 +6,27 @@
  * Excel sheet column number
  */
 public class Leet_58 {
-    public static void solve() {
+    public static int solve() {
+        String input = "    fly me   to   the moon  ";
+        int result = 0;
+//        for(String s : input.trim().split("\\s+")) {
+//            System.out.println(s);
+//        }
 
+        StringTokenizer st = new StringTokenizer(input, " ");
+
+        System.out.println(st.countTokens());
+        while(st.hasMoreTokens())
+            result = st.nextToken().length();
+
+        return result;
     }
+
+    public static void main(String[] args) {
+        System.out.println(solve());
+    }
+    /**
+     * https://www.javamex.com/tutorials/regular_expressions/splitting_tokenisation_performance.shtml
+     *
+     */
 }
