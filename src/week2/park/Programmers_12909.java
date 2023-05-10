4f4ae8e1a5c2a56bfae590ffff3a43465a6c9a12
Joonwoo Park <93jpark@gmail.com>
2023-05-10 19:41:34 +0900

    solved

--- src/week2/park/Programmers_12909.java
+++ src/week2/park/Programmers_12909.java
@@ -1,2 +1,32 @@
+import java.util.*;
+
 public class Programmers_12909 {
+    private static boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        if(s.length() % 2 == 1) return false;
+
+        for(char c : s.toCharArray()) {
+            if(c == ')') {
+                if(stack.isEmpty()) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            } else if (c == '(') {
+                stack.push(c);
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(")(((("));  // false
+        System.out.println(solution("(())"));   // true
+        System.out.println(solution("()()"));   // true
+        System.out.println(solution("(())()")); // true
+        System.out.println(solution(")()("));   // false
+        System.out.println(solution("(()("));   // false
+    }
 }
