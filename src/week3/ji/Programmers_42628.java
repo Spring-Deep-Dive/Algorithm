380514e126de4da165220695b0bbd2ce255a49ab
Seunghyeon Ji <85978996+jee14@users.noreply.github.com>
2023-05-23 20:14:57 +0900

    Adjust indentation

--- src/week3/ji/Programmers_42628.java
+++ src/week3/ji/Programmers_42628.java
@@ -2,37 +2,37 @@ import java.util.*;
 
 class Solution {
 
-	private Queue<Integer> minQueue = new PriorityQueue<>();
-	private Queue<Integer> maxQueue = new PriorityQueue<>(Comparator.reverseOrder());
+    private Queue<Integer> minQueue = new PriorityQueue<>();
+    private Queue<Integer> maxQueue = new PriorityQueue<>(Comparator.reverseOrder());
 
-	public int[] solution(String[] operations) {
-		int[] answer = new int[2];
+    public int[] solution(String[] operations) {
+        int[] answer = new int[2];
 
-		for (int i = 0; i < operations.length; i++) {
-			String[] pair = operations[i].split(" ");
-			Integer element;
-			switch (pair[0]) {
-				case "I" -> {
-					element = Integer.parseInt(pair[1]);
-					minQueue.offer(element);
-					maxQueue.offer(element);
-				}
-				case "D" -> {
-					if (pair[1].equals("1")) {
-						element = maxQueue.poll();
-						minQueue.remove(element);
-					} else {
-						element = minQueue.poll();
-						maxQueue.remove(element);
-					}
-				}
-			}
+	for (int i = 0; i < operations.length; i++) {
+            String[] pair = operations[i].split(" ");
+	    Integer element;
+	    switch (pair[0]) {
+                case "I" -> {
+		    element = Integer.parseInt(pair[1]);
+		    minQueue.offer(element);
+		    maxQueue.offer(element);
 		}
-		Integer max = maxQueue.poll();
-		Integer min = minQueue.poll();
-		answer[0] = max != null ? max : 0;
-		answer[1] = min != null ? min : 0;
-
-		return answer;
+		case "D" -> {
+		    if (pair[1].equals("1")) {
+		        element = maxQueue.poll();
+			minQueue.remove(element);
+		    } else {
+			element = minQueue.poll();
+			maxQueue.remove(element);
+		    }
+		}
+	    }
 	}
+	Integer max = maxQueue.poll();
+	Integer min = minQueue.poll();
+	answer[0] = max != null ? max : 0;
+	answer[1] = min != null ? min : 0;
+
+	return answer;
+    }
 }
