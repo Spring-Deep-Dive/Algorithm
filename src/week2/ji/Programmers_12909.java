461daf036aafb8f3e441ce0b2ae24652ee3f33a8
Seunghyeon Ji <85978996+jee14@users.noreply.github.com>
2023-05-23 20:10:44 +0900

    Adjust indentation

--- src/week2/ji/Programmers_12909.java
+++ src/week2/ji/Programmers_12909.java
@@ -1,17 +1,17 @@
 class Solution {
     boolean solution(String s) {
         int cnt = 0;
-		for (int i = 0; i < s.length(); i++) {
-			if (cnt < 0) {
-				break;
-			}
-			if (s.charAt(i) == '(') {
-				cnt++;
-			} else {
-				cnt--;
-			}
-		}
-
-		return cnt == 0;
+	for (int i = 0; i < s.length(); i++) {
+	    if (cnt < 0) {
+	        break;
+	    }
+	    if (s.charAt(i) == '(') {
+	        cnt++;
+	    } else {
+	        cnt--;
+	    }
+	}
+	    
+	return cnt == 0;
     }
 }
