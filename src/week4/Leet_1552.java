e2617f1b859913b00903ef12fc2aabdef11ffac5
Seunghyeon Ji <85978996+jee14@users.noreply.github.com>
2023-05-25 15:28:25 +0900

    Create Leet_1552.java

---
+++ src/week4/Leet_1552.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+class Solution {
+
+	private static final int MAX = Integer.MIN_VALUE;
+
+	public int maxDistance(int[] position, int m) {
+		Arrays.sort(position);
+		int start = 1;
+		int end = position[position.length - 1] - position[0];
+		int result = 0;
+
+		while (start <= end) {
+			int mid = start + (end - start) / 2;
+			if (isFeasible(position, m, mid)) {
+				result = mid;
+				start = mid + 1;
+			} else {
+				end = mid - 1;
+			}
+		}
+
+		return result;
+	}
+
+	private boolean isFeasible(int[] position, int m, int gap) {
+		int count = 1;
+		int prev = position[0];
+
+		for (int i = 1; i < position.length; i++) {
+			if (position[i] - prev >= gap) {
+				count++;
+				prev = position[i];
+			}
+		}
+
+		return count >= m;
+	}
+}
